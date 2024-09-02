4a5a8180c53b28a15db7ab676ee20a96bb7a20b1
Toby Thomas <toby@tobythomas.tech>
2024-09-02 16:34:35 +0100

    Methods to extract wind and precipitation data.

--- source/netcdf.pxd
+++ source/netcdf.pxd
@@ -1,6 +1,7 @@
 cimport numpy as np
 from cpython.datetime cimport datetime
 
+
 cdef class NetCDF_wave:
     cdef datetime _epoch_time
     # Longitude range
@@ -20,6 +21,48 @@ cdef class NetCDF_wave:
     cdef np.ndarray _dp 
     # Methods
     cdef tuple __get_wave_data_at(self, double longitude, double latitude, int time)
+
+
+cdef class NetCDF_wind:
+    cdef datetime _epoch_time
+    # Longitude range
+    cdef double _first_longitude
+    cdef double _last_longitude
+    cdef double _delta_longitude
+    # Latitude range
+    cdef double _first_latitude
+    cdef double _last_latitude
+    cdef double _delta_latitude
+    # Time range
+    cdef double _first_time
+    cdef double _last_time
+    cdef double _delta_time
+    # Numpy array of wind velocities.
+    cdef np.ndarray _u # Eastward component of wind velocity (m/s)
+    cdef np.ndarray _v # Northward component of wind velocity (m/s)
+    # Methods
+    cdef tuple __get_wind_speed_at(self, double longitude, double latitude, int time)
+
+
+cdef class NetCDF_precipitation:
+    cdef datetime _epoch_time
+    # Longitude range
+    cdef double _first_longitude
+    cdef double _last_longitude
+    cdef double _delta_longitude
+    # Latitude range
+    cdef double _first_latitude
+    cdef double _last_latitude
+    cdef double _delta_latitude
+    # Time range
+    cdef double _first_time
+    cdef double _last_time
+    cdef double _delta_time
+    # Numpy array for total precipitation.
+    cdef np.ndarray _tp # Total precipitation (m)
+    # Methods
+    cdef double __get_precipitation_at(self, double longitude, double latitude, int time)
+
 
 cdef class NetCDF_current:
     cdef datetime _epoch_time
--- source/netcdf.pyx
+++ source/netcdf.pyx
@@ -9,7 +9,7 @@ cdef class NetCDF_wave:
     '''
     def __cinit__(self, file_path) -> None:
         data = nc.Dataset(file_path)
-        self._epoch_time = datetime(1900, 1, 1)  
+        self._epoch_time = datetime(1900, 1, 1)
         # Longitude range
         self._first_longitude = data.variables["longitude"][0].item()
         self._last_longitude = data.variables["longitude"][-1].item()
@@ -87,6 +87,128 @@ cdef class NetCDF_wave:
             return (wave_hs, wave_dp) # m, radians
 
 
+cdef class NetCDF_wind:
+    '''
+    Class to extract wind data from an .nc file.
+    '''
+    def __cinit__(self, file_path) -> None:
+        data = nc.Dataset(file_path)
+        self._epoch_time = datetime(1900, 1, 1)
+        # Longitude range
+        self._first_longitude = data.variables["longitude"][0].item()
+        self._last_longitude = data.variables["longitude"][-1].item()
+        self._delta_longitude = data.variables["longitude"][1].item() - data.variables["longitude"][0].item()
+        # Latitude range
+        self._first_latitude = data.variables["latitude"][0].item()
+        self._last_latitude = data.variables["latitude"][-1].item()
+        self._delta_latitude = data.variables["latitude"][1].item() - data.variables["latitude"][0].item()
+        # Time range
+        self._first_time = data.variables["time"][0].item()
+        self._last_time = data.variables["time"][-1].item()
+        self._delta_time = data.variables["time"][1].item() - data.variables["time"][0].item()
+        # Numpy array of eastward and northward wind velocities.
+        self._u = data.variables["u10"][:].data # numpy array of dtype=float32 of shape (len_time, len_latitude, len_longitude)
+        self._v = data.variables["v10"][:].data # numpy array of dtype=float32 of shape (len_time, len_latitude, len_longitude)
+
+    cdef tuple __get_wind_speed_at(self, double longitude, double latitude, int time_diff_from_epoch_in_hrs):
+        # Check if latitude, longitude and time are within range of the data in the netcdf file.
+        if not ((self._first_latitude <= latitude <= self._last_latitude) or (self._first_latitude >= latitude >= self._last_latitude)):
+            raise ValueError("Latitude {} is out of range [{}, {}]".format(latitude, self._first_latitude, self._last_latitude))
+        if not ((self._first_longitude <= longitude <= self._last_longitude) or (self._first_longitude >= longitude >= self._last_longitude)):
+            raise ValueError("Longitude {} is out of range [{}, {}]".format(longitude, self._first_longitude, self._last_longitude))
+        cdef datetime time
+        cdef datetime time_0
+        cdef datetime time_n
+        if not ((self._first_time <= time_diff_from_epoch_in_hrs <= self._last_time) or (self._first_time >= time_diff_from_epoch_in_hrs >= self._last_time)):
+            time   = self._epoch_time + timedelta(hours=int(time_diff_from_epoch_in_hrs))
+            time_0 = self._epoch_time + timedelta(hours=int(self.times[0]))
+            time_n = self._epoch_time + timedelta(hours=int(self.times[-1]))
+            raise ValueError("Time {} is out of range [{}, {}]".format(time, time_0, time_n))
+        # Index
+        cdef int index_time = int((time_diff_from_epoch_in_hrs - self._first_time) // self._delta_time)
+        cdef int index_latitude = round((latitude - self._first_latitude) / self._delta_latitude)
+        cdef int index_longitude = round((longitude - self._first_longitude) / self._delta_longitude)
+        cdef double u = self._u[index_time, index_latitude, index_longitude]
+        cdef double v = self._v[index_time, index_latitude, index_longitude]
+        return (u, v)
+
+    def get_wind_speed_at(self, longitude, latitude, time) -> tuple:
+        '''
+        Returns the eastward and northward component of wind velocities for a given location and time.
+        '''
+        # Get diff from epoch time in hrs
+        time_diff_from_epoch = time - self._epoch_time # time difference w.r.t epoch time
+        time_diff_from_epoch_in_sec = time_diff_from_epoch.total_seconds()
+        time_diff_from_epoch_in_hrs = divmod(time_diff_from_epoch_in_sec, 3600)[0]
+        # NetCDF files have longitude in the range [-180.0, 179.5]. Therefore if the given longitude > 179.5, then round to nearest 0.5
+        if longitude >= 179.75 and longitude <= 180.0:
+            longitude = -180.0
+        elif longitude > 179.5 and longitude < 179.75:
+            longitude = 179.5
+        u, v = self.__get_wind_data_at(longitude, latitude, time_diff_from_epoch_in_hrs)
+        return (u, v) # m/s
+
+
+cdef class NetCDF_precipitation:
+    '''
+    Class to extract precipitation data from an .nc file.
+    '''
+    def __cinit__(self, file_path) -> None:
+        data = nc.Dataset(file_path)
+        self._epoch_time = datetime(1900, 1, 1)
+        # Longitude range
+        self._first_longitude = data.variables["longitude"][0].item()
+        self._last_longitude = data.variables["longitude"][-1].item()
+        self._delta_longitude = data.variables["longitude"][1].item() - data.variables["longitude"][0].item()
+        # Latitude range
+        self._first_latitude = data.variables["latitude"][0].item()
+        self._last_latitude = data.variables["latitude"][-1].item()
+        self._delta_latitude = data.variables["latitude"][1].item() - data.variables["latitude"][0].item()
+        # Time range
+        self._first_time = data.variables["time"][0].item()
+        self._last_time = data.variables["time"][-1].item()
+        self._delta_time = data.variables["time"][1].item() - data.variables["time"][0].item()
+        # Numpy array of total precipitation.
+        self._tp = data.variables["tp"][:].data # numpy array of dtype=float32 of shape (len_time, len_latitude, len_longitude)
+
+    cdef double __get_precipitation_at(self, double longitude, double latitude, int time_diff_from_epoch_in_hrs):
+        # Check if latitude, longitude and time are within range of the data in the netcdf file.
+        if not ((self._first_latitude <= latitude <= self._last_latitude) or (self._first_latitude >= latitude >= self._last_latitude)):
+            raise ValueError("Latitude {} is out of range [{}, {}]".format(latitude, self._first_latitude, self._last_latitude))
+        if not ((self._first_longitude <= longitude <= self._last_longitude) or (self._first_longitude >= longitude >= self._last_longitude)):
+            raise ValueError("Longitude {} is out of range [{}, {}]".format(longitude, self._first_longitude, self._last_longitude))
+        cdef datetime time
+        cdef datetime time_0
+        cdef datetime time_n
+        if not ((self._first_time <= time_diff_from_epoch_in_hrs <= self._last_time) or (self._first_time >= time_diff_from_epoch_in_hrs >= self._last_time)):
+            time   = self._epoch_time + timedelta(hours=int(time_diff_from_epoch_in_hrs))
+            time_0 = self._epoch_time + timedelta(hours=int(self.times[0]))
+            time_n = self._epoch_time + timedelta(hours=int(self.times[-1]))
+            raise ValueError("Time {} is out of range [{}, {}]".format(time, time_0, time_n))
+        # Index
+        cdef int index_time = int((time_diff_from_epoch_in_hrs - self._first_time) // self._delta_time)
+        cdef int index_latitude = round((latitude - self._first_latitude) / self._delta_latitude)
+        cdef int index_longitude = round((longitude - self._first_longitude) / self._delta_longitude)
+        cdef double tp = self._tp[index_time, index_latitude, index_longitude]
+        return tp
+
+    def get_precipitation_at(self, longitude, latitude, time) -> tuple:
+        '''
+        Returns the total precipitation for a given location and time.
+        '''
+        # Get diff from epoch time in hrs
+        time_diff_from_epoch = time - self._epoch_time # time difference w.r.t epoch time
+        time_diff_from_epoch_in_sec = time_diff_from_epoch.total_seconds()
+        time_diff_from_epoch_in_hrs = divmod(time_diff_from_epoch_in_sec, 3600)[0]
+        # NetCDF files have longitude in the range [-180.0, 179.5]. Therefore if the given longitude > 179.5, then round to nearest 0.5
+        if longitude >= 179.75 and longitude <= 180.0:
+            longitude = -180.0
+        elif longitude > 179.5 and longitude < 179.75:
+            longitude = 179.5
+        tp = self.__get_precipitation_at(longitude, latitude, time_diff_from_epoch_in_hrs)
+        return tp # m
+
+
 cdef class NetCDF_current:
     '''
     Class to extract zonal and meridional velocities from a .nc file.
@@ -126,7 +248,6 @@ cdef class NetCDF_current:
         cdef double zonal_velocity = self._zonal_velocities[index_time, index_latitude, index_longitude]
         cdef double meridional_velocity = self._meridional_velocities[index_time, index_latitude, index_longitude]
         return (zonal_velocity, meridional_velocity)
-
 
     def get_ocean_current_at(self, longitude, latitude, time) -> tuple:
         '''
